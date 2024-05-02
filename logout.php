6748e2906cc6205a59621bc5e37668f1d160a25f
53rj <163045696+53rj@users.noreply.github.com>
2024-05-02 11:56:33 +0200

    Progress
    so far

---
+++ logout.php
@@ -0,0 +1,19 @@
+<?php
+include "include/meta.html";
+include "include/header.html";
+?>
+<title>Logout</title>
+
+<?php
+session_start();
+$_SESSION = array();
+if (ini_get("session.use_cookies")) { 
+    $params = session_get_cookie_params(); 
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    ); 
+}
+session_destroy(); 
+include "include/footer.html";
+?>
