766d51de93fa13e306b9695dd8cdba8d538da1a1
53rj <163045696+53rj@users.noreply.github.com>
2024-05-02 14:07:00 +0200

    updates
    Logout Update
    uantrag erstellung
    header update nach uantrag
    korrektur von Marcos Namen

--- logout.php
+++ logout.php
@@ -6,14 +6,14 @@ include "include/header.html";
 
 <?php
 session_start();
-$_SESSION = array();
-if (ini_get("session.use_cookies")) { 
-    $params = session_get_cookie_params(); 
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    ); 
-}
+$_SESSION = array();                                //////////////////////////////
+if (ini_get("session.use_cookies")) {               //                          //
+    $params = session_get_cookie_params();          //                          //
+    setcookie(session_name(), '', time() - 42000,   //        ChatGPT           //
+        $params["path"], $params["domain"],         //                          //
+        $params["secure"], $params["httponly"]      //                          //
+    );                                              //                          //
+}                                                   //////////////////////////////
 session_destroy(); 
 include "include/footer.html";
 ?>
---
+++ uantrag.php
@@ -0,0 +1,17 @@
+<?php
+include "include/meta.html";
+include "include/header.html";
+?>
+<body>
+<div class="main_content">
+    <br>
+    <label for="urlaub">Urlaubsbeginn:</label>
+    <input type="date" id="urlaub" name="ubeginn">
+    <label for="urlaub">Urlaubsende:</label>
+    <input type="date" id="urlaub" name="uende">
+    <button type="submit" name="uantrag" id="submit">Urlaub beantragen</button>
+</div>
+</body>
+<?php
+include "include/footer.html";
+?>
