ef4a5a4a157026e7f79ccaf06fa4a01eff4d40b6
blackbarthel <greatjamsession@gmail.com>
2024-05-02 14:05:24 +0200

    Klasse krank u. urlaubsantrag erstellt

--- function.php
+++ function.php
@@ -1,7 +1,6 @@
 <?php
 
 class personal{
-
     private $pid;
     private $vorname;
     private $nachname;
@@ -45,14 +44,12 @@ class personal{
         $this->nachname = $newName;
     }
 
-
     public function getPasswort(){
         return $this->passwort;
     }
     public function setPasswort($newPasswort){
         $this->passwort = $newPasswort;
     }
-
 
     public function getStatus(){
         return $this->status;
@@ -61,14 +58,12 @@ class personal{
         $this->status = $newStatus;
     }
 
-
     public function getUid(){
         return $this->uid;
     }
     public function setUid($newUid){
         $this->uid = $newUid;
     }
-
 
     public function getKid(){
         return $this->kid;
@@ -77,7 +72,6 @@ class personal{
         $this->kid = $newKid;
     }
 
-
     public function getUtage(){
         return $this->utage;
     }
@@ -85,14 +79,142 @@ class personal{
         $this->utage = $newUtage;
     }
 
-
     public function getRestutage(){
         return $this->restutage;
     }
     public function setRestutage($newRestutage){
         $this->restutage = $newRestutage;
     }
+}
 
+class krank{
+    private $kid;
+    private $pid;
+    private $kanfang;
+    private $kende;
+    private $kgesamt;
+    
+    public function __construct($kid,$pid,$kanfang,$kende,$kgesamt){
+        $this->kid = $kid;
+        $this->pid = $pid;
+        $this->kanfang = $kanfang;
+        $this->kende = $kende;
+        $this->kgesamt = $kgesamt;
+
+        public function getKid(){
+            return $this->kid;
+        }
+        public function setKid($newKid){
+            $this->kid = $newKid;
+        }
+
+        public function getPid(){
+            return $this->pid;
+        }
+        public function setPid($newPid){
+            $this->pid = $newPid;
+        }
+
+        public function getKanfang(){
+            return $this->kanfang;
+        }
+        public function setKanfang($newKanfang){
+            $this->kanfang = $newKanfang;
+        }
+
+        public function getKende(){
+            return $this->kende;
+        }
+        public function setKende($newKende){
+            $this->kende = $newKende;
+        }
+
+        public function getKgesamt(){
+            return $this->kgesamt;
+        }
+        public function setKgesamt($newKgesamt){
+            $this->kgesamt = $newKgesamt;
+        }
+    }
+}
+
+class urlaubsantrag{
+    private $uid;
+    private $pid;
+    private $uanfang;
+    private $uende;
+    private $ubeantragt;
+    private $ugenommen;
+    private $ugesamt;
+    private $ustatus;
+    
+    public function __construct($uid,$pid,$uanfang,$uende,$ubeantragt,$ugenommen,$ugesamt,$ustatus){
+        $this->uid = $uid;
+        $this->pid = $pid;
+        $this->uanfang = $uanfang;
+        $this->uende = $uende;
+        $this->ubeantragt = $ubeantragt;
+        $this->ugenommen = $ugenommen;
+        $this->ugesamt = $ugesamt;
+        $this->ustatus = $ustatus;
+
+
+        public function getUid(){
+            return $this->uid;
+        }
+        public function setUid($newUid){
+            $this->uid = $newUid;
+        }
+
+        public function gePid(){
+            return $this->pid;
+        }
+        public function setPid($newPid){
+            $this->pid = $newPid;
+        }
+
+        public function getUanfang(){
+            return $this->uanfang;
+        }
+        public function setUanfang($newUanfang){
+            $this->uanfang = $newUanfang;
+        }
+
+        public function getUende(){
+            return $this->uende;
+        }
+        public function setUende($newUende){
+            $this->uende = $newUende;
+        }
+
+        public function getUbeantragt(){
+            return $this->ubeantragt;
+        }
+        public function setUbeantragt($newUbeantragt){
+            $this->ubeantragt = $newUbeantragt;
+        }
+
+        public function getUgenommen(){
+            return $this->ugenommen;
+        }
+        public function setUgenommen($newUgenommen){
+            $this->ugenommen = $newUgenommen;
+        }
+
+        public function getUgesamt(){
+            return $this->ugesamt;
+        }
+        public function setUgesamt($newUgesamt){
+            $this->ugesamt = $newUgesamt;
+        }
+
+        public function getUstatus(){
+            return $this->ustatus;
+        }
+        public function setUstatus($newUstatus){
+            $this->ustatus = $newUstatus;
+        }
+    }
 }
 
 function connServer(){
@@ -128,7 +250,7 @@ function checkuser(){
 }
 
 
-function showData($zahl){
+function showAllData($zahl){
     connServer();
     
     if ($zahl == 1){
