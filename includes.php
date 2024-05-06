9980263de292b3e7423eac73e3e1bd64a618f76a
C1V4N <168530202+C1V4N@users.noreply.github.com>
2024-05-06 17:46:22 +0200

    Add files via upload
    in includes sind alle dateien verlinkt, die in Sergiys order waren.
    Testlabor beinhaltet alle Funkrionen, die ich als fertig indentifizieren konnte.
    f_function braucht einen neuen namen...!

---
+++ includes.php
@@ -0,0 +1,24 @@
+<?php
+
+// Finale Dateien und Klassen
+include "style.css";
+include "registration.js";
+include "f_function.php";
+
+include "personal_class.php";
+include "urlaub_class.php";
+include "krank_class.php";
+
+
+// Hier sind provisorische Dateien included
+
+include "function.php";
+include "logout.php";
+include "php.php";
+include "index.php";
+include "register.php";
+include "uanfrage.php";
+include "uantrag.php";
+include "udbv.php";
+include "ugenehmigen,.php";
+include "test.html";
